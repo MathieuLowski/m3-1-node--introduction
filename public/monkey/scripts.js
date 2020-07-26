b3dd461d933222743a05799821c0526cbc91abf8
Mathieu Szydlowski <mathieu.lowski@gmail.com>
2020-07-26 13:05:33 -0400

    up to 4.3

--- public/monkey/scripts.js
+++ public/monkey/scripts.js
@@ -1,5 +1,5 @@
-const messageInput = document.querySelector('#user-input');
-const conversationElem = document.querySelector('#conversation-container');
+const messageInput = document.querySelector("#user-input");
+const conversationElem = document.querySelector("#conversation-container");
 
 // focus the input on load
 const handleFocus = () => {
@@ -8,28 +8,34 @@ const handleFocus = () => {
 
 // updateConversation expects an object with 'user' and 'text'
 const updateConversation = (message) => {
-  const { author, text } = message;
-  const messageElem = document.createElement('p');
+  console.log(message);
+  //what i have { status: 200, message:'hi'}
+  // what i want {text:'hi'}
+  //const { author, text } = message;
+  const author = message.author;
+  const text = message.text;
+  const messageElem = document.createElement("p");
 
-  messageElem.classList.add('message', author);
+  messageElem.classList.add("message", author);
   messageElem.innerHTML = `<span>${text}</span>`;
   conversationElem.appendChild(messageElem);
   conversationElem.scrollTop = conversationElem.scrollHeight;
 
-  if (author === 'user') messageInput.value = '';
+  if (author === "user") messageInput.value = "";
   handleFocus();
 };
 
 const sendMessage = (event) => {
   event.preventDefault();
 
-  const message = { author: 'user', text: messageInput.value };
+  const message = { author: "user", text: messageInput.value };
   updateConversation(message);
 
-  fetch('/monkey-message')
+  fetch("/monkey-message")
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
+      // data = { status: 200, message:'hi'}
       updateConversation(data.message);
     });
 };
--- server.js
+++ server.js
@@ -1,29 +1,88 @@
-'use strict';
+"use strict";
 
 // import the needed node_modules.
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const { query } = require("express");
 
 express()
   // Below are methods that are included in express(). We chain them for convenience.
   // --------------------------------------------------------------------------------
 
   // This will give us will log more info to the console. see https://www.npmjs.com/package/morgan
-  .use(morgan('tiny'))
+  .use(morgan("tiny"))
 
   // Any requests for static files will go into the public folder
-  .use(express.static('public'))
+  .use(express.static("public"))
 
   // Nothing to modify above this line
   // ---------------------------------
   // add new endpoints here 👇
+  .get("/cat-message", (req, res) => {
+    const message = { author: "cat", text: "Meow" };
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get("/monkey-message", (req, res) => {
+    const messageArray = [
+      "Don't monkey around me",
+      "🐵",
+      "If you pay peanuts, you get monkeys",
+      "🙈",
+      "I fling 💩 at you!",
+      "🙉",
+    ];
+
+    const allMessage = [Math.floor(Math.random() * messageArray.length)];
+    const message = { authpor: "monkdey", text: messageArray[allMessage] };
+    const randomTime = Math.floor(Math.random() * 4000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  //fetch("/parrot-message?text=hello")
+  .get("/parrot-message", (req, res) => {
+    //console.log(req.query);
+    let parrotAnsw = req.query.text;
+    const message = { author: "parrot", text: parrotAnsw }; //If changing author:"parrot" to "user" message will b rendered on user side.
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
+
+  .get("/bot-message", (req, res) => {
+    console.log(req.query.text);
+    const getBotMessage = (text) => {
+      const commonGreetings = ["hi", "hello", "howdy"];
+      const commonGoodbyes = ["ciao", "bye", "goodbye", "peace"];
+      let botMsg = "";
+      if (commonGreetings.includes(text.toLowerCase())) {
+        botMsg = "Hello!";
+      } else if (commonGoodbyes.includes(text.toLowerCase())) {
+        botMsg = "Au revoir my friend";
+      } else botMsg = "Bzzt " + req.query.text;
+      return botMsg;
+    };
+
+    let botAnsw = getBotMessage(req.query.text);
+    const message = { author: "bot", text: botAnsw }; //If changing author:"parrot" to "user" message will b rendered on user side.
+    const randomTime = Math.floor(Math.random() * 3000);
+    setTimeout(() => {
+      res.status(200).json({ status: 200, message });
+    }, randomTime);
+  })
 
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
 
   // this serves up the homepage
-  .get('/', (req, res) => {
+  .get("/", (req, res) => {
     res
       .status(200)
       .json({ status: 200, message: "This is the homepage... it's empty :(" });
@@ -31,13 +90,11 @@ express()
 
   // this is our catch all endpoint. If a user navigates to any endpoint that is not
   // defined above, they get to see our 404 page.
-  .get('*', (req, res) => {
-    res
-      .status(404)
-      .json({
-        status: 404,
-        message: 'This is obviously not the page you are looking for.',
-      });
+  .get("*", (req, res) => {
+    res.status(404).json({
+      status: 404,
+      message: "This is obviously not the page you are looking for.",
+    });
   })
 
   // Node spins up our server and sets it to listen on port 8000.
